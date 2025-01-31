8066da59a690481622177ed9c74fa3051e6ae3f0
Soumya <Sonisoumya2003@gmail.com>
2025-01-31 14:28:11 +0530

    updated description componrnt

--- client/src/components/description.jsx
+++ client/src/components/description.jsx
@@ -1,9 +1,20 @@
 // import React from 'react'
 
+import { assets } from "../assets/assets"
+
 const Description = () => {
   return (
-    <div>
-        <h1>Create AI Images</h1>
+    <div className="flex flex-col items-center justify-center my-24 p-6 md:px-28">
+        <h1 className="text-3xl sm:text-4xl font-semibold mb-2">Create AI Images</h1>
+        <p className="text-gray-500 mb-8">Turn your imagination into visuals</p>
+
+        <div>
+          <img src={assets.sample_img_1} alt="" className="w-80 xl:w-96 rounded-lg" />
+          <div>
+            <h2>Introducing the AI-Powered Text to Image Generator</h2>
+            <p>Easily bring your ideas to life with our free AI image Generator. Whether you need stunning visuals or unique imagery, our tool transforms your text into eye-catching images with just a few clicks. Imagine it, describe it, and watch it come to life instantly.</p>
+          </div>
+        </div>
     </div>
   )
 }
