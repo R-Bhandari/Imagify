784fc7a7f617e02a8d35380366c8701bb047708d
Tanishka <103167586+TAANISHKA@users.noreply.github.com>
2025-03-29 11:12:14 +0530

    Fixed the API and can generate the image through image generation button.

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext()
 
@@ -11,6 +12,8 @@ const AppContextProvider = (props) => {
 
     const [credit, setCredit] =useState(false)
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const navigate = useNavigate()
 
     const loadCreditsData = async () => {
         try {
@@ -28,6 +31,29 @@ const AppContextProvider = (props) => {
         }
 
     }
+
+    const generateImage = async (prompt) => {
+        try {
+            const {data} = await axios.post(backendUrl + '/api/image/generate-image', {prompt}, {headers: {token}})
+
+            if(data.success){
+                loadCreditsData()
+                return data.resultImage
+            } else {
+                toast.error(data.message)
+                loadCreditsData()
+                if(data.creditBalance === 0){
+                    navigate('/buy')
+                }
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+
+
     const logout = ()=>{
         localStorage.removeItem('token');
         setToken('')
@@ -41,7 +67,7 @@ const AppContextProvider = (props) => {
     },[token])
 
     const value = {
-        user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit, loadCreditsData, logout
+        user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit, loadCreditsData, logout, generateImage
     }
 
     return (
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,7 +1,8 @@
 // import React from 'react'
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { assets } from "../assets/assets"
 import { motion } from "motion/react"
+import { AppContext } from "../context/AppContext"
 
 const Result = () => {
 
@@ -10,8 +11,20 @@ const Result = () => {
   const [loading, setLoading] = useState(true)
   const [input, setInput] = useState('')
 
-  const onSubmitHandler = async (e) => {
+  const {generateImage} = useContext(AppContext)
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    if(input){
+      const image = await generateImage(input)
+      if(image){
+        setIsImageLoaded(true)
+        setImage(image)
+      }
+    }
+    setLoading(false)
   }
 
   return (
